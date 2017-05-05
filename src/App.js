0fd42f3970af9095e8d656f5481468019b1dcf38
Valerie Barela <vbarela1@gmail.com>
2017-05-05 10:56:38 -0600

    adds increment decrement

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import Hello from './Hello';
 
 class App extends Component {
+  state = { counter: 0 };
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  decrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>{ this.state.counter }</h1>
+        <button onClick={ this.increment }>+</button>
+        <button onClick={ this.decrement }>-</button>
       </div>
     );
   }
